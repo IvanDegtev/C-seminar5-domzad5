691ebfe6ee8cf4a2cda2125e288b39ad86d6c828
Ivan <degtev1990@mail.ru>
2023-04-15 09:28:26 +0300

    задание 1 решение окончание

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -3,3 +3,52 @@
 // чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+int duble(int[] array)
+{
+    int chek = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (1 <= array[i] & array[i] < 1000)
+            if (array[i] % 2 == 0)
+            {
+                chek++;
+            }
+            else
+            {
+
+            }
+    }
+    return chek;
+}
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной ведичины");
+int maxRnd = ReadInt("Введите границу максимума случайной ведичины");
+int[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+int per;
+per = duble(array);
+System.Console.WriteLine($" Количество четных чисел в массиве {per} ");
