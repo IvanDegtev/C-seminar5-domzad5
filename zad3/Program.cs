5f063ce48b289a382c1d87e1bb6b8a98c232aab9
Ivan <degtev1990@mail.ru>
2023-04-25 21:37:11 +0300

    задача 3 решение

--- zad3/Program.cs
+++ zad3/Program.cs
@@ -2,3 +2,54 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+double[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    double[] array = new double[length];
+    Random rnd = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+double SumSign(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return max - min;
+}
+
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной величины");
+int maxRnd = ReadInt("Введите границу максимума случайной величины");
+double[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+System.Console.WriteLine($"Разница между максимальным и минимальным элементов массива {SumSign(array)}");
