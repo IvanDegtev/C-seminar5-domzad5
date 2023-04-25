c122ff00ee9fe9a53a888d54494e1b6e41cf2b60
Ivan <degtev1990@mail.ru>
2023-04-25 21:34:51 +0300

    задача 2 решение

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -4,3 +4,54 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int SumSign(int[] array, bool isPositive = true)
+{
+    int sign = 1;
+    if (!isPositive)
+    {
+        sign = -1;
+    }
+
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        if (array[i] * sign < 0 || sign > 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной величины");
+int maxRnd = ReadInt("Введите границу максимума случайной величины");
+int[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+System.Console.WriteLine($"Сумма элементов стоящих на нечетных позициях {SumSign(array)}");
